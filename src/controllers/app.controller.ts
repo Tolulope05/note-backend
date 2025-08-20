1e454c9494babf445e71f89f5c8b6c44699ede6a
Tolulope Fakunle <fakunletolulope05@gmail.com>
2025-08-20 12:14:03 +0100

    Added Admin priviledged info

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, HttpCode, Post, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Post,
+  Query,
+  Redirect,
+  Req,
+} from '@nestjs/common';
 import { AppService } from '../app.service';
 
 @Controller()
@@ -32,9 +41,34 @@ export class NoteController {
   createNote(): object {
     return { status: 201, body: `This action creates a note` };
   }
+
+  // TO UPDATE RESPONSE CODE
   @Post('admin')
   @HttpCode(204)
   createAdmin() {
-    return 'This action adds a new cat';
+    return 'This action adds a new note';
+  }
+
+  // TO SEND BACK HEADER TO THE REQUEST, DO THIS
+  @Post('adminhead')
+  @Header('Cache-Control', 'no-store')
+  create() {
+    return 'This action adds a new note v2';
+  }
+
+  // A REDIRECT REQUEST
+  @Get('priviledgedinfo')
+  @Redirect('https://nestjs.com', 301)
+  accessPriviledgedInfo() {
+    return 'This is unsafe';
+  }
+
+  // A CONDITIONAL REQUEST
+  @Get('docs')
+  @Redirect('https://google.com', 302)
+  getDocs(@Query('version') vsn) {
+    if (vsn && vsn == '5') {
+      return { url: 'https://nestjs.com' };
+    }
   }
 }
